0fb1739f33480dbfb63239ea3ff1e77ecd5fc436
Thomas Reifenberger <tom-mi@users.noreply.github.com>
2025-11-04 23:40:26 +0100

    fix: Fix options flow to update config entry instead of options

--- custom_components/sungrow/options_flow.py
+++ custom_components/sungrow/options_flow.py
@@ -5,11 +5,16 @@ from homeassistant.const import CONF_SCAN_INTERVAL
 class SungrowInverterOptionsFlow(OptionsFlow):
 
     async def async_step_init(self, user_input=None):
+        # hack to adapt config instead of options, see https://community.home-assistant.io/t/configflowhandler-and-optionsflowhandler-managing-the-same-parameter/365582/4
         if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
+            self.hass.config_entries.async_update_entry(
+                self.config_entry,
+                data={**self.config_entry.data, **user_input},
+            )
+            return self.async_create_entry(title="", data={})
 
         options_schema = vol.Schema({
-            vol.Optional(CONF_SCAN_INTERVAL, default=self.config_entry.options.get(CONF_SCAN_INTERVAL, 30)): int,
+            vol.Optional(CONF_SCAN_INTERVAL, default=self.config_entry.data.get(CONF_SCAN_INTERVAL, 60)): int,
         })
 
         return self.async_show_form(
